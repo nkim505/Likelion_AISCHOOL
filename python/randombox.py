fcb199fc6f6bcd8a05a109b08aa4845ade805723
Nari Kim <59485802+nkim505@users.noreply.github.com>
2022-03-15 19:54:16 +0900

    Update randombox.py

--- python/randombox.py
+++ python/randombox.py
@@ -1,16 +1,16 @@
 # This is written by myself applying the AI SCHOOL contents to comply with their copyright issues.
 # Purpose: To make a random box to ramdomly choose who's gonna pay the dinner for all.
-# 본 코드는 교육 내용을 응용하여 새롭게 작성되었습니다.
+# 본 코드는 멋쟁이 사자처럼에서 진행하는 AI SCHOOL의 교육 내용을 응용하여 새롭게 작성되었습니다.
 # 목표: 저녁식사를 결제할 한 사람을 뽑는 랜덤박스를 만들어보기.
 
 import random
 import time
 
-names = ['미영', '지훈', '민지', '도윤', '상하']
+names = ['Ryun', 'Apeach', 'Donald', 'Roman', 'Eden']
 
 while True:
     print(names)
-    item = input("추가할 멤버의 이름을 입력하세요. (끝내려면 q):")
+    item = input("Please input new member's name (to exit: q):")
     if item == "q":
         break
     else:
@@ -21,19 +21,19 @@ set_names = set(names)
 
 while True:
     print(set_names)
-    item = input("삭제할 멤버의 이름을 입력해주세요. (끝내려면 q):")
+    item = input("Please input a name you want to delete from the list (to exit: q):")
     if item =='q':
         break
     else:
         set_names = set_names - set([item])
 print(set_names)
 
-print(set_names, "중에서 오늘 식사를 결제할 한 사람을 고릅니다.")
+print("The machine will choose one person among the list :", set_names)
 
 for i in range(5):
     print(5-i)
     time.sleep(1)
     
 final_return = random.choice(list(set_names))
-print("랜덤 선택 결과는 " + final_return + "(이)가 오늘 식사를 결제하게 되었습니다. 축하 박수~~ 짝짝짝")
-print("프로그램을 종료합니다.")
+print("The result is... " + final_return + "is chosen to pay the dinner. Clap clap clap!")
+print("The applicaion closed.")
